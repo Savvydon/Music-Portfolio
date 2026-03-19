9a804646e9e117b2a52bdf90dd15a53cb6a7069e
Savvydon <donsave4real@gmail.com>
2026-03-19 13:27:45 +0100

    initial commit

--- music-portfolio/src/components/NavigationBar.jsx
+++ music-portfolio/src/components/NavigationBar.jsx
@@ -1,34 +1,85 @@
+// import { Navbar, Nav, Container } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import logo from "../assets/logo.jpg";
+
+// export default function NavigationBar() {
+//   return (
+//     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+//       <Container>
+//         <Navbar.Brand as={Link} to="/">
+//           <img
+//             src={logo}
+//             alt="Logo"
+//             width="60"
+//             height="60"
+//             className="d-inline-block align-top me-2"
+//           />
+//           Crown Piano Studio
+//         </Navbar.Brand>
+//         <Navbar.Toggle />
+//         <Navbar.Collapse>
+//           <Nav className="ms-auto">
+//             <Nav.Link as={Link} to="/">
+//               Home
+//             </Nav.Link>
+//             <Nav.Link as={Link} to="/about">
+//               About
+//             </Nav.Link>
+//             <Nav.Link as={Link} to="/services">
+//               Services
+//             </Nav.Link>
+//             <Nav.Link as={Link} to="/contact">
+//               Contact
+//             </Nav.Link>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+import { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jpg";
 
-export default function NavigationBar() {
+export default function Navigation() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      expanded={expanded} // control collapse state
+      className="mb-4"
+    >
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img
-            src={logo}
-            alt="Logo"
-            width="60"
-            height="60"
-            className="d-inline-block align-top me-2"
-          />
-          Crown Piano Studio
+          MyWebsite
         </Navbar.Brand>
-        <Navbar.Toggle />
+        <Navbar.Toggle
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+        />
         <Navbar.Collapse>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link as={Link} to="/about" onClick={() => setExpanded(false)}>
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/services">
+            <Nav.Link
+              as={Link}
+              to="/services"
+              onClick={() => setExpanded(false)}
+            >
               Services
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
+            <Nav.Link
+              as={Link}
+              to="/contact"
+              onClick={() => setExpanded(false)}
+            >
               Contact
             </Nav.Link>
           </Nav>
--- music-portfolio/src/pages/Home.jsx
+++ music-portfolio/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import heroImg from "../assets/opened.jpg";
 import promoVideo from "../assets/vid1.mp4";
-import promoVideo2 from "../assets/vid2.mp4";
+import promoVideo2 from "../assets/Vid2.mp4";
 import promoVideo3 from "../assets/vid3.mp4";
 
 export default function Home() {
